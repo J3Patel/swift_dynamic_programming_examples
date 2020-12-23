96a92e80b681ea0ec5ac4558624d4c1d138d74d4
J3Patel <jatinpatel101@gmail.com>
2020-12-23 12:32:55 +0530

    Added can sum problem

--- dynamic_programming_memoization.playground/Contents.swift
+++ dynamic_programming_memoization.playground/Contents.swift
@@ -1,43 +1,60 @@
 
-// MARK:  - Fibonacci problem
+// MARK:  - CanSum problem
 
 
 // MARK: Solution 1 - Brute force
 /**
- Time Complexity: O(2^n)
- Space Complexity: O(n)
+ Time Complexity: O(n^m)
+ Space Complexity: O(m) // as call stack would use
  */
-func fib(num: Int) -> Int {
-    guard num > 2 else {
-        return 1
+func canSum(targetSum: Int, numbers: [Int]) -> Bool {
+    guard targetSum >= 0 else {
+        return false
     }
-    return fib(num: num - 1) + fib(num: num - 2)
+    if targetSum == 0 { return true }
+    
+    for n in numbers {
+        if canSum(targetSum: targetSum - n, numbers: numbers) {
+            return true
+        }
+    }
+
+    return false
 }
 
 
-print(fib(num: 6))
-print(fib(num: 7))
-print(fib(num: 8))
-// print(fib(num: 50)) // this will take time
+print(canSum(targetSum: 7, numbers: [1,2]))
+print(canSum(targetSum: 7, numbers: [5,3,4,7]))
+print(canSum(targetSum: 7, numbers: [2,4]))
+print(canSum(targetSum: 8, numbers: [2,3,5]))
+//print(canSum(targetSum: 300, numbers: [14,7])) will take LOT of time
 
 
 // MARK: Solution 2 - Memoized
 /**
- Time Complexity: O(n)
+ Time Complexity: O(n*m)
  Space Complexity: O(n)
  */
-var memo: [Int:Int] = [:]
-func fibMemoized(num: Int) -> Int {
-    if let val = memo[num] { return val }
-    guard num > 2 else {
-        return 1
+var memo: [Int:Bool] = [:]
+func canSumMemoized(targetSum: Int, numbers: [Int]) -> Bool {
+    if let val = memo[targetSum] { return val }
+    guard targetSum >= 0 else {
+        return false
     }
-    memo[num] = fibMemoized(num: num - 1) + fibMemoized(num: num - 2)
-    return memo[num]!
+    
+    if targetSum == 0 { return true }
+    for n in numbers {
+        if canSumMemoized(targetSum: targetSum - n, numbers: numbers) {
+            memo[targetSum] = true
+            return true
+        }
+    }
+    memo[targetSum] = false
+    return false
 }
 
-print(fibMemoized(num: 6))
-print(fibMemoized(num: 7))
-print(fibMemoized(num: 8))
-print(fibMemoized(num: 50))
-
+print(canSumMemoized(targetSum: 7, numbers: [1,2]))
+print(canSumMemoized(targetSum: 7, numbers: [5,3,4,7]))
+print(canSumMemoized(targetSum: 7, numbers: [2,4]))
+print(canSumMemoized(targetSum: 8, numbers: [2,3,5]))
+print(canSumMemoized(targetSum: 300, numbers: [14,7]))
